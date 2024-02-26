e0274da255d933eee77e9f6118835ae230eec3ad
MD. SAJID HASSAN <sajidhasan2023@gmail.com>
2024-02-26 17:02:11 +0600

    FTT

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="cashier-copyright-area">
         <div className="cashier-copyright text-center bg-themeBlue bg-footer h-20 leading-[80px] mt-20">
           <span className="text-[15px] text-white font-normal">
-            © Copyright by BDevs - 2023
+           FutureTech Tycoons
           </span>
         </div>
       </div>
